76130178097f35c2cb5c6546b566daea10972378
fritzj2 <fritzj2@berea.edu>
2025-11-15 15:16:34 -0500

    Added the start of the Character Class, it can draw cards

---
+++ Character.py
@@ -0,0 +1,43 @@
+'''
+
+'''
+import pygame
+import random
+
+class Character:
+    def __init__(self):
+        self.money = 500
+        self.card_values = 0
+        self.number_of_cards = 0
+        self.number_of_aces = 0
+
+    def card_picker(self):
+        card = random.randint(1,13)
+        suit = random.randint(1,4)
+        if card == 1:
+            print('you drew a {0}'.format(card))
+            self.number_of_aces += 1
+            self.card_values += 11
+        elif 1 < card <= 10:
+            print('you drew a {0}'.format(card))
+            self.card_values += card
+        else:
+            print('you drew a {0}'.format(card))
+            self.card_values += 10
+
+        if self.card_values > 21:
+            if self.number_of_aces >= 1:
+                print('ace is now 1 not 11')
+                self.card_values -= 10
+                self.number_of_aces -= 1
+            else:
+                print('You Lose')
+                return
+        print(self.card_values)
+
+# player = Character()
+# player.card_picker()
+# player.card_picker()
+# player.card_picker()
+
+
