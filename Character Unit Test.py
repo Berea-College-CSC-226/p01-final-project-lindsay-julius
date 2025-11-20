e81d1c70f9d20484843c7a7e98a65db128020d03
manabatl3 <lindsaym4206@gmail.com>
2025-11-19 19:33:41 -0500

    Started writing the unittest for Character class

---
+++ Character Unit Test.py
@@ -0,0 +1,20 @@
+# import unittest
+#
+#
+# class MyTestCase(unittest.TestCase):
+#     def test_something(self):
+#         self.assertEqual(True, False)  # add assertion here
+#
+#
+#
+# if __name__ == '__main__':
+#     unittest.main()
+
+from inspect import getframeinfo, stack
+
+from Character import *
+
+
+
+
+
