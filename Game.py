3852f620ef770d0a771264b2c364358b83a1d8c7
manabatl3 <lindsaym4206@gmail.com>
2025-11-22 16:29:02 -0500

    Tried to implement the force lose if the player hits over 21, still need to make double make sure it's doing what its supposed to.

--- Game.py
+++ Game.py
@@ -14,7 +14,7 @@ class Game:
         self.clock = pygame.time.Clock()
         self.user = Player.Player()
         self.cpu = Dealer.Dealer()
-        self.turn = 'dealer'
+        self.turn = 'player'
 
     def turn_of_play(self):
         while self.running:
@@ -24,18 +24,45 @@ class Game:
             # The code above allows the game to quit
 
             if  self.turn == 'player':
-                print('players turn')
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        print('players turn to draw a card')
+                    #add draw function here
                 # Above are the functions and methods that the player can call
+                    if self.user.card_values > 21:
+                        print(f'You lost! Total is {self.user.card_values}.')
+                        self.turn = 'game over'
 
-            elif self.turn == 'dealer':
-                self.cpu.dealers_turn()
-                self.showdown()
-                #These are the actions that the dealer will take
 
-            else:
-                print('game end')
-                #Logic at the end of the game that will allow the player to replay the game.
 
+            elif event.key == pygame.K_s: # s stands fors stand
+                self.turn = 'dealer'
+
+
+        if self.turn == 'plater':
+            pass
+
+
+        elif self.turn == 'dealer':
+            print("Dealer's turn..")
+            self.cpu.dealers_turn()
+            self.showdown()
+            self.turn = 'game_over'
+
+        elif self.turn == 'game_over':
+            pass
+
+        else:
+            print('game end')
+            # elif self.turn == 'dealer':
+            #     self.cpu.dealers_turn()
+            #     self.showdown()
+            #     #These are the actions that the dealer will take
+            #
+            # else:
+            #     print('game end')
+            #     #Logic at the end of the game that will allow the player to replay the game.
+            #
             pygame.display.update()
             self.clock.tick(24)
             # Handles the screen not crashing
@@ -43,8 +70,10 @@ class Game:
 
 
     def showdown(self):
-        if self.user.card_values > self.cpu.card_values:
-            print('You win!')
+        if self.user.card_values > 21:
+            print('You Lose!')
+        elif self.user.card_values > self.cpu.card_values:
+            print('You Win!')
         else:
             print('You lose!')
 
