f71cf4fd53bc6fe5586fd40c4e017207500c01fc
fritzj2 <fritzj2@berea.edu>
2025-11-22 15:59:58 -0500

    Makes the screen somewhat work and added part of the no ones turn to the running loop

--- Game.py
+++ Game.py
@@ -11,21 +11,36 @@ class Game:
         self.running = True
         self.screen = pygame.display.set_mode(self.size)
         self.screen.fill('#00850b')
+        self.clock = pygame.time.Clock()
         self.user = Player.Player()
         self.cpu = Dealer.Dealer()
-        self.turn = 'player'
+        self.turn = 'dealer'
 
     def turn_of_play(self):
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+            # The code above allows the game to quit
 
-            while self.turn == 'player':
-                pass
-                #self.user.card_picker()
-            self.cpu.dealers_turn()
-            self.showdown()
+            if  self.turn == 'player':
+                print('players turn')
+                # Above are the functions and methods that the player can call
+
+            elif self.turn == 'dealer':
+                self.cpu.dealers_turn()
+                self.showdown()
+                #These are the actions that the dealer will take
+
+            else:
+                print('game end')
+                #Logic at the end of the game that will allow the player to replay the game.
+
+            pygame.display.update()
+            self.clock.tick(24)
+            # Handles the screen not crashing
+
+
 
     def showdown(self):
         if self.user.card_values > self.cpu.card_values:
