63165678b61ae5c30a0a072142af354b2cf4d396
manabatl3 <lindsaym4206@gmail.com>
2025-12-09 15:17:53 -0500

    Removed comments from the player file

--- Player.py
+++ Player.py
@@ -9,9 +9,6 @@ class Player(Character.Character):
     def __init__(self):
         super().__init__()
 
-    # def Stand(self):
-    #     player_turn = 'dealer'
-    # Unused code for now, if it needs to be reimplemented it can
     def Double(self):
         if self.card_values >= 11:
             self.card_picker()
